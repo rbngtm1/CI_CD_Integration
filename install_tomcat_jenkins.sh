4fdfbbc07b8afa6c6bf27705db84ebe5ff4bd8f4
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-02-09 22:09:30 -0600

    Create install_tomcat_jenkins.sh

---
+++ install_tomcat_jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# run the script in /opt
+sudo yum install tomcat
+sudo vi /usr/share/tomcat/conf/tomcat.conf
+echo 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx1026m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"'>>/usr/share/tomcat/conf/tomcat.conf
+# install the web application manager and the documentation,
+sudo yum install tomcat-webapps tomcat-admin-webapps
+sudo yum install tomcat-docs-webapp tomcat-javadoc
+sudo cat /usr/share/tomcat/conf/tomcat-users.xml | sed 's/<tomcat-users>/<tomcat-users>\n<user username="admin" password="password" roles="manager-gui,admin-gui"/> > /usr/placeholder.xml
+sudo cp /usr/placeholder.xml /usr/share/tomcat/conf/tomcat-users.xml
+# start the service and to ensure that the service is started when Centos powers on
+sudo service tomcat start
+sudo chkconfig tomcat on
+
+
+sudo wget https://updates.jenkins-ci.org/latest/jenkins.war
+sudo cp jenkins.war /opt/tomcat9/webapps/
