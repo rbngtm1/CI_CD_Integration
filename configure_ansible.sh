0061f089bb3575dace1b8cdf1bc19b2ad33a97f8
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-02-10 23:57:32 -0600

    Update configure_ansible.sh

--- configure_ansible.sh
+++ configure_ansible.sh
@@ -4,12 +4,13 @@ sudo yum install -y epel-release
 sudo yum-config-manager --enable epel -y
 sudo yum repolist -y
 sudo yum update -y
-sudo yum install git python python-devel python-pip openssl ansible
+sudo yum install -y git python python-devel python-pip openssl ansible
 ansible --version
 sudo bash -c "cat /etc/ansible/ansible.cfg | sed 's/basic default values.../basic default values...\ninventory      = \/etc\/ansible\/hosts/g' | sed 's/basic default values...\nsudo_user     = root/g' > /etc/ansible/ansible.cfg.replacement"
-sudo mv /etc/ansible/ansible.cfg.replacement /etc/ansible/ansible.cfg
+sudo cp /etc/ansible/ansible.cfg.replacement /etc/ansible/ansible.cfg
 sudo mv /etc/ansible/hosts /etc/hosts.original
 sudo bash -c 'echo "[local]" > /etc/ansible/hosts' && sudo bash -c 'echo "localhost" >> /etc/ansible/hosts'
 sudo useradd ansible
-echo 'ansible' | sudo -S passwd ansible # Note the password of ansible user here is ansible
-sudo bash -c "cat /etc/sudoers | sed 's/.*group to run networking.*/ansible    ALL=(ALL)    NOPASSWD: ALL\n\n&/' | sed 's/.*group to run networking.*/ec2-user    ALL=(ALL)    NOPASSWD: ALL\n\n&/' > /etc/sudoers"
+#echo 'ansible' | sudo -S passwd ansible # Note the password of ansible user here is ansible
+sudo bash -c "cat /etc/sudoers | sed 's/.*group to run networking.*/ansible    ALL=(ALL)    NOPASSWD: ALL\n\n&/' | sed 's/.*group to run networking.*/ec2-user    ALL=(ALL)    NOPASSWD: ALL\n\n&/' > /etc/sudoers.placeholder"
+sudo cp /etc/sudoers.placeholder /etc/sudoers
