2e5cd6abfb7c39e2ecf213a05d368ff30cddce8d
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-02-12 20:53:07 -0600

    Update and rename sth to maven.sh

---
+++ maven.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum install java -y
+sudo wget https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+sudo tar xzvf apache-maven-3.6.3-bin.tar.gz
+sudo echo "PATH=$PATH:/opt/apache-maven-3.6.3/bin" > ~/.bash_rc
+source ~/.bash_rc
