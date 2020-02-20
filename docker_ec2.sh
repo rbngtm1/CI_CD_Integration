89a506b0d928ef212aedcbbd9da9d7b73690b309
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-02-19 22:07:35 -0600

    Update docker_ec2.sh

--- docker_ec2.sh
+++ docker_ec2.sh
@@ -5,5 +5,7 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo docker info
+echo "============================"
+echo "Please enter the dockerhub password"
 read -s password
 docker login -u "rbngtm1" -p "$password" docker.io
